7fed451482f2ec38c211556aa666306493cadef1
morellowhammy <morellowhammy@hotmail.com>
2018-10-28 12:52:12 +0100

    Created a new component inside login module: ng g c login/loggingin

--- my-app/src/app/login/login.module.ts
+++ my-app/src/app/login/login.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LogginginComponent } from './loggingin/loggingin.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [LogginginComponent]
 })
 export class LoginModule { }
