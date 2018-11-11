cad3a8f501a0e417da5c2de642461adff9e47dbb
morellowhammy <morellowhammy@hotmail.com>
2018-11-11 13:51:40 +0100

    Created Component inside Admin Module: ng g c admin/email-blast

--- angular-routing/src/app/admin/admin.module.ts
+++ angular-routing/src/app/admin/admin.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { EmailBlastComponent } from './email-blast/email-blast.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule
   ],
-  declarations: [AdminComponent]
+  declarations: [AdminComponent, EmailBlastComponent]
 })
 export class AdminModule { }
