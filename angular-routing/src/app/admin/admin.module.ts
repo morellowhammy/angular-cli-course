750f6cad234f2d2be7dc4e42647d83ba49658f66
morellowhammy <morellowhammy@hotmail.com>
2018-11-11 13:52:39 +0100

    Created Users component in admin module: ng g c admin/users

--- angular-routing/src/app/admin/admin.module.ts
+++ angular-routing/src/app/admin/admin.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { EmailBlastComponent } from './email-blast/email-blast.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule
   ],
-  declarations: [AdminComponent, EmailBlastComponent]
+  declarations: [AdminComponent, EmailBlastComponent, UsersComponent]
 })
 export class AdminModule { }
